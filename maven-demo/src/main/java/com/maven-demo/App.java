408b1bc6a3665b4e542816e8de441cc6bcf51a03
Boris Tsigelman <borist@develeap-bt-mbp.local>
2022-02-12 22:27:22 +0200

    Updated maven demo project

--- maven-demo/src/main/java/com/maven-demo/App.java
+++ maven-demo/src/main/java/com/maven-demo/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        System.out.println( "Develeap's Maven & Artifactory Demo!" );
     }
 }
